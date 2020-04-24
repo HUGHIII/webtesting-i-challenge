8a9179b03e92f8bbec025c0d8887a9438eb5fb83
TREY <hugh.a.lloyd@gmail.com>
2020-04-23 22:29:45 -0500

    added succeed func tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,41 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  return {
+    ...item,
+    enhancement: item.enhancement < 20 ? item.enhancement + 1 : 20,
+  };
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return { ...item, durability: item.durability - 5 };
+  } else if (item.enhancement >= 15) {
+    if (item.enhancement > 16) {
+      return {
+        ...item,
+        durability: item.durability - 10,
+        enhancement: item.enhancement - 1,
+      };
+    }
+    return { ...item, durability: item.durability - 10 };
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  if (
+    !item ||
+    typeof item.name === "undefined" ||
+    typeof item.durability === "undefined" ||
+    typeof item.enhancement === "undefined"
+  ) {
+    throw new error("item must have name, durability and enhancement");
+  } else if (item.durability > 100 || item.durability < 0) {
+    throw new error("durability must be from 1-100");
+  } else if (item.enhancement > 20 || item.enhancement < 0) {
+    throw new error("item enhancement must be from 1-20");
+  }
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,77 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("repair()  tests", function () {
+  it("should return object with -durability- property = 100 ", function () {
+    const item1Damaged = {
+      name: "stormDaemon",
+      durability: 50,
+      enhancement: 20,
+    };
+
+    const item1Repaired = {
+      name: "stormDaemon",
+      durability: 100,
+      enhancement: 20,
+    };
+
+    const item2Damaged = {
+      name: "plagueClawCatapult",
+      durability: 40,
+      enhancement: 20,
+    };
+
+    const item2Repaired = {
+      name: "plagueClawCatapult",
+      durability: 100,
+      enhancement: 20,
+    };
+
+    expect(enhancer.repair(item1Damaged)).toStrictEqual(item1Repaired);
+    expect(enhancer.repair(item2Damaged)).toStrictEqual(item2Repaired);
+    expect(enhancer.repair(item1Repaired)).toStrictEqual(item1Repaired);
+  });
+  it("should throw error when not given a proper object", function () {
+    const brokenObject = {
+      name: "warpFireThrower",
+      enhancement: 20,
+    };
+    expect(() => enhancer.repair(brokenObject)).toThrow();
+  });
+
+  it("should throw error if durability isnt 1-100", function () {
+    const brokenDurability = {
+      name: "warpFireThrower",
+      enhancement: 20,
+      durability: 101,
+    };
+    expect(() => enhancer.repair(brokenDurability)).toThrow();
+  });
+
+  it("should throw error if enhancement isnt 1-20", function () {
+    const brokenEnhancement = {
+      name: "warpFireThrower",
+      enhancement: 22,
+      durability: 100,
+    };
+
+    expect(() => enhancer.repair(brokenEnhancement)).toThrow();
+  });
+});
+
+describe("succeed() tests", function () {
+  it("should add 1 to enhancement if its less than 20", function () {
+    const enhancement18 = {
+      name: "warpFireThrower",
+      enhancement: 18,
+      durability: 100,
+    };
+
+    const enhancement19 = {
+      name: "warpFireThrower",
+      enhancement: 19,
+      durability: 100,
+    };
+    expect(enhancer.succeed(enhancement18)).toStrictEqual(enhancement19);
+  });
+});
